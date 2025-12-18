e3f282bd0c58e7b48f7eed1ab3016d09f032e16b
Ujjal Roy <royu99099@gmail.com>
2025-12-18 15:51:57 +0530

    exploring get, post , delete

--- homework.md
+++ homework.md
@@ -1,1 +1,12 @@
-learn about package.json and package-lock.json
+-learn about package.json and package-lock.json
+
+-initialize git 
+-gitignore
+-create a remote repo on github
+-push all code to github origin
+
+-played with routes (wildcard matching)
+-lesson:- order of the routes matters a lot
+-episode-04:- postman
+-write logic to handle GET , POST , PUT , PATCH , DELETE API calls
+-
--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use("/test" , (req , res)=>{
-  res.send("Hello World!!!Test route working");
-})
+
+
+app.get("/user" , (req , res) => {
+    res.send({firstname : "John" , lastname: "Doe"}); 
+});
+
+app.post("/user" , (req , res) => {
+    res.send("Data successfully posted!"); 
+});
+
+app.delete("/user" , (req , res) => {
+    res.send("User successfully deleted!"); 
+});
+
+app.use("/test" , (req , res) => {
+    res.send("Test route is working!"); 
+});
 
 app.listen(5000 , () => {
     console.log("Server is running on port 5000");
