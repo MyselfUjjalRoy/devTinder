87a58d3b019470bc5754811a56085d3905ed54b9
Ujjal Roy <royu99099@gmail.com>
2026-01-05 22:37:42 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:userId", (req, res) => {
-    console.log(req.params);
-    res.send({ firstname: "John", lastname: "Doe" });
-});
+app.get("/user" ,(req , res , next)=>{
+    console.log("Handling the route user 1")
+     next();
+},
+(req , res , next)=>{
+    //res.send("Route Hadler 2");
+    console.log("Handling the route user 2");
+    next();
+},
+(req , res , next)=>{
+    console.log("Handling the route user 3");
+    next();
+},
+(req , res)=>{
+    res.send("Route Hadler 4");
+}
+);
 
 
 
 
-app.listen(5000 , () => {
-    console.log("Server is running on port 5000");
+app.listen(7777 , () => {
+    console.log("Server is running on port 7777");
 });
